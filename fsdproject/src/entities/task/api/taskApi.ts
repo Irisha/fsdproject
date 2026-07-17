4400435bce3935651fefd8aa63d62183fbf42a1f
Ирина Цамутали <i.bashaeva@gmail.com>
2026-07-17 15:59:21 +0300

    3 first tasks of leson 3

---
+++ fsdproject/src/entities/task/api/taskApi.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { Task } from '../model/types';
+
+export const tasksApi = createApi({
+  reducerPath: 'tasksApi',
+  
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: 'https://jsonplaceholder.typicode.com' 
+  }),
+  tagTypes: ['Tasks'],
+  endpoints: (build) => ({
+    getTasks: build.query<Task[], void>({
+      query: () => 'todos',
+      transformResponse: (response: Task[]) => response,
+    }),
+  }),
+});
+
+export const { useGetTasksQuery } = tasksApi;
--- fsdproject/src/main.tsx
+++ fsdproject/src/main.tsx
@@ -1,16 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux';
+import {ThemeProvider} from '@gravity-ui/uikit';
+import App from './app/App.tsx'
+import { store } from 'app/store/store.ts';
 import './index.css'
 import '@gravity-ui/uikit/styles/fonts.css';
 import '@gravity-ui/uikit/styles/styles.css';
-import {ThemeProvider} from '@gravity-ui/uikit';
-import App from './app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
 
   <ThemeProvider theme="light">
     <StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </StrictMode>
   </ThemeProvider>,
 )
